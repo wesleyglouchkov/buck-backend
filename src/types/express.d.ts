8de0f27822d393297d521b2222e0d21fd9ba09ec
Amcelo13 <tchetan308@gmail.com>
2025-12-08 19:02:54 -0500

    ts node render

---
+++ src/types/express.d.ts
@@ -0,0 +1,28 @@
+import { User } from '@prisma/client';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+      cookies: {
+        [key: string]: string;
+      };
+      headers: {
+        [key: string]: string | string[] | undefined;
+      };
+    }
+  }
+}
+
+export interface AuthenticatedRequest extends Express.Request {
+  user: User;
+  body: any;
+  params: any;
+  query: any;
+  cookies: {
+    [key: string]: string;
+  };
+  headers: {
+    [key: string]: string | string[] | undefined;
+  };
+}
