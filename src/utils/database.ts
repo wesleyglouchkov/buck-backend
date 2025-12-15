96f5637bc1359852b71c2aab0345dcfec8445f88
Amcelo13 <tchetan308@gmail.com>
2025-12-15 02:33:37 -0500

    feat: Upgrade Prisma to v7 and integrate `@prisma/adapter-pg` for dynamic database connection.

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,11 +1,23 @@
+import 'dotenv/config';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const connectionStringDev = `${process.env.DATABASE_URL_DEV}`;
+const isDevEnvironment = process.env.NODE_ENV === 'development';
+
+const pool = new Pool({ connectionString: isDevEnvironment ? connectionStringDev : connectionString });
+const adapter = new PrismaPg(pool);
+
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 export const db = globalForPrisma.prisma ?? new PrismaClient({
-  log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+  adapter,
+  log: isDevEnvironment ? ['error', 'warn'] : ['error'],
 });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
